3f671e05ec9fb6286a968589ccf562c370878207
diazdavidd <42588561+diazdavidd@users.noreply.github.com>
2025-08-06 13:01:39 -0500

    Remove source files that interfere with deployment - serve only built files

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import { BreathingExercise } from './components/BreathingExercise';
-
-export default function App() {
-  return (
-    <div style={{ width: '100%', height: '100vh' }}>
-      <BreathingExercise />
-    </div>
-  );
-}
